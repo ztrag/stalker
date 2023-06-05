b7ccd32ed7f7bc5aeb696818b3a4cd7780e8a9d8
ztrag <ztrag@protonmail.com>
2023-06-04 23:00:26 -0700

    marker with target position

--- lib/map/map_page.dart
+++ lib/map/map_page.dart
@@ -1,32 +1,82 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:stalker/db/db.dart';
 import 'package:stalker/domain/stalk_target.dart';
 import 'package:stalker/stalk/stalk_machine_widget.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   final StalkTarget stalkTarget;
 
   const MapPage({Key? key, required this.stalkTarget}) : super(key: key);
 
   @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  late StreamSubscription<StalkTarget?> targetDbSubscription;
+  StalkTarget? updatedTarget;
+
+  @override
+  void initState() {
+    super.initState();
+    _monitorTarget();
+  }
+
+  @override
+  void dispose() {
+    targetDbSubscription.cancel();
+    super.dispose();
+  }
+
+  void _monitorTarget() async {
+    final db = await Db.db;
+    targetDbSubscription =
+        db.stalkTargets.watchObject(widget.stalkTarget.id).listen((event) {
+      setState(() {
+        updatedTarget = event;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final t = updatedTarget ?? widget.stalkTarget;
     return StalkMachineWidget(
-      stalkTarget: stalkTarget,
+      stalkTarget: widget.stalkTarget,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('stalking ${stalkTarget.name ?? ''}'),
+          title: Text('stalking ${widget.stalkTarget.name ?? ''}'),
         ),
-        body: Platform.isIOS
+        body: Platform.isIOS || !_hasLocation(t)
             ? Container()
-            : const GoogleMap(
+            : GoogleMap(
+                markers: {
+                  Marker(
+                    markerId: MarkerId('${widget.stalkTarget.id}'),
+                    position: LatLng(
+                      t.lastLocationLatitude!,
+                      t.lastLocationLongitude!,
+                    ),
+                  ),
+                },
                 initialCameraPosition: CameraPosition(
-                  target: LatLng(45.5312571, -122.6984473),
+                  target: LatLng(
+                    widget.stalkTarget.lastLocationLatitude!,
+                    widget.stalkTarget.lastLocationLongitude!,
+                  ),
                   zoom: 14,
                 ),
               ),
       ),
     );
+  }
+
+  bool _hasLocation(StalkTarget target) {
+    return target.lastLocationLongitude != null &&
+        target.lastLocationLatitude != null;
   }
 }
